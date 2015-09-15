e138cfebbebb5f10f005fdf1334dd5d084f46308
liebesu <lichunjiang42@gmail.com>
2015-09-15 10:41:59 +0800

    create link
    create link file

--- modules/signatures/create_newfile.py
+++ modules/signatures/create_newfile.py
@@ -9,4 +9,14 @@ class Cmd(Signature):
     authors = ["liebesu"]
     minimum = "1.2"
     def run(self):
+        file_indicators=["\\\*.link"]
+        found = True
+     for indicator in file_indicators:
+            file_match = self.check_file(pattern=indicator, regex=True, all=True)
+            if file_match:
+                for match in file_match:
+                    self.data.append({"file" : match })
+                found = True
+        return found
+
 
